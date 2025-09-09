c54bdf995f82da029850711c7d7d231975d1c62f
suyeon <paksy0428@gmail.com>
2025-09-09 15:43:58 +0900

    feat: Axios 기본 인스턴스(api) 설정 추가

--- placeit-frontend/src/lib/axios.ts
+++ placeit-frontend/src/lib/axios.ts
@@ -1,13 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-const ACCESS_TOKEN = process.env.NEXT_PUBLIC_DEV_ACCESS_TOKEN;
-
 export const api = axios.create({
-  baseURL: API_BASE_URL,
-});
-
-api.interceptors.request.use(config => {
-  config.headers.Authorization = `Bearer ${ACCESS_TOKEN}`;
-  return config;
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  withCredentials: true,
 });
